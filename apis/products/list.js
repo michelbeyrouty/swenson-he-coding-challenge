016bbad3d5e4719f119ec2736a7bfe8971681fbe
Michel Beyrouty <michelbeyrouty@gmail.com>
2021-05-18 23:16:09 +0300

    Fix constants

--- apis/products/list.js
+++ apis/products/list.js
@@ -4,7 +4,7 @@ module.exports.list = (event, context, callback) => {
 
     const { globals:app } = require("../../globals");
     const SKU_LIST = Object.values(app.constants.SKU_LIST);
-    let SKU_NAMES_LIST = SKU_LIST.map(product => product.NAME);
+    let SKU_NAMES_LIST = SKU_LIST.map(product => product.name);
     let productsFilterd = [] ;
 
     const inputData = JSON.parse(event.body);
@@ -16,11 +16,9 @@ module.exports.list = (event, context, callback) => {
         requiredInputs: app.constants.VALIDATION.REQUIRED.listProduct
      });
 
-    for( inputField of Object.keys(inputData) ){
-        if(inputField){
+    for(inputField of Object.keys(inputData)){
           productsFilterd = SKU_LIST.filter(product => product[inputField] == inputData[inputField]).map(product => product.name);
-          SKU_NAMES_LIST = _.intersection(productsFilterd,SKU_NAMES_LIST)
-        }
+          SKU_NAMES_LIST = app._.intersection(productsFilterd,SKU_NAMES_LIST)
     }
 
     const response = {
